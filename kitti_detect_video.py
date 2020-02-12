852f450fd612c52ed76967dd8840baa11b84dc60
Soto Anno <>
2020-02-12 18:55:12 +0900

    misc

--- kitti_detect_video.py
+++ kitti_detect_video.py
@@ -9,9 +9,9 @@ image_folder = '../kitti/data_tracking_image_2/training/image_02/0000'
 result_path = "result.mp4"
 
 yolo = YOLO()
-img_rows = 812
-img_cols = 416
-img_size = (832, 416)
+# img_rows = 812
+# img_cols = 416
+# img_size = (832, 416)
 
 images = [img for img in os.listdir(image_folder) if img.endswith(".png") and img[0] != "."]
 images.sort()
@@ -24,7 +24,7 @@ print(os.path.join(image_folder, images[0]))
 
 for i, image in enumerate(images):
 	frame = Image.open(os.path.join(image_folder, image))
-	frame = frame.resize(img_size)
+	# frame = frame.resize(img_size)
 	detected = yolo.detect_image(frame)
 	detected.save('output/0000/%05d.png' % i)
 	#video.write(np.asarray(detected))
