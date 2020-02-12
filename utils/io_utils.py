e0ccbf4f8309533d9645142e4804139fd13c7204
Soto Anno <>
2020-02-12 18:55:48 +0900

    change return format

--- utils/io_utils.py
+++ utils/io_utils.py
@@ -5,6 +5,11 @@ persons_class = ["1"]
 
 kitti_classes = ['Car', 'Van', 'Truck', 'Pedestrian', 'Person_sitting', 'Cyclist', 'Tram']
 
+"""
+Created by kyomin on 
+Modified by anno on 2020/02/12
+"""
+
 def xywh2xyxy(bbox):
     """
     convert bbox from [x,y,w,h] to [x1, y1, x2, y2]
@@ -31,7 +36,8 @@ def reorder_frameID(frame_dict):
         new_dict[str(key)] = frame_dict[str(key)]
     return new_dict
 
-def read_txt_gt4mot17det(textpath):
+def read_txt_gt4V(textpath):
+    # TODO: this method will be no longer used
     """
     read gt.txt in MOT17Det dataset to a dict
     :param textpath: text path, string
@@ -46,6 +52,28 @@ def read_txt_gt4mot17det(textpath):
                 line = line[0].split(' ')
             # we only consider "pedestrian" class #
             if len(line) < 7 or (line[7] not in persons_class and "MOT2015" not in textpath) or int(float(line[6]))==0:
+                continue
+            if not (line[0]) in frames:
+                frames[line[0]] = []
+            bbox = xywh2xyxy(line[2:6])
+            frames[line[0]].append([line[1]]+bbox)
+    ordered = reorder_frameID(frames)
+    return ordered
+
+def read_txt_gt4mot17det(textpath):
+    """
+    read gt.txt in MOT17Det dataset to a dict
+    :param textpath: text path, string
+    :return: a dict with key = frameid and value is a tuple (class name str, [x1, y1, x2, y2]) in the frame, dict
+    """
+    # line format : [frameid, personid, x, y, w, h ...]
+    with open(textpath) as f:
+        f_csv = csv.reader(f)
+        frames = {}
+        for line in f_csv:
+            if len(line) == 1:
+                line = line[0].split(' ')
+            if len(line) < 7 or ("MOT2015" not in textpath) or int(float(line[6]))==0:
                 continue
             if not (line[0]) in frames:
                 frames[line[0]] = []
@@ -81,24 +109,29 @@ def read_txt_gt4kitti(textpath):
 
     This dataset contains the following classes.
     ['Car', 'Van', 'Truck', 'Pedestrian', 'Person_sitting', 'Cyclist', 'Tram', 'Misc' or 'DontCare']
+    These classes will be converted into the following number labeling.
+    [0, 1, 2, 3, 4, 5, 6 (, 7)]
+    The class "'Misc' or 'DontCare'" will be ignored.
 
     :param textpath: text path, string
-    :return: a dict with key = frameid and value is a list of lists [object id, x1, y1, x2, y2] in the frame, dict
+    :return: a dict with key = frameid and value is a tuple (class name str, [x1, y1, x2, y2]) in the frame, dict
     """
 
     with open(textpath) as f:
         frames = {}
         for line in f.readlines():
-            if len(line) == 1:
-                line = line[0].split(" ")
+            line = line.rstrip()
+            line = line.split(" ")
+            # print(line)
             if len(line) < 17 or line[2] in ['Misc', 'DontCare']:
                 continue
             if not (line[0]) in frames:
                 frames[line[0]] = []
             # frame[6:10] -> [left, top, height, bottom] in String
             bbox = [float(line[6]), float(line[7]), float(line[8]), float(line[9])]
-            object_id = kitti_classes.index(line[2])
-            frames[line[0]].append([object_id]+bbox)
+            # object_id = kitti_classes.index(line[2])
+            object_class = line[2]
+            frames[line[0]].append((object_class, bbox))
     ordered = reorder_frameID(frames)
     return ordered
 
