9cb3b91c1e82ec76e5ddec5151a55c2c51b274a9
Soto Anno <>
2020-02-13 19:27:13 +0900

    fix typo

--- retraining.py
+++ retraining.py
@@ -218,7 +218,7 @@ class Trainer(object):
         for line in annotation_lines:
             line = line.split()
             image_id = line[0]
-            line_boxes_ids = [l.sprit(",") for l in line[1:]]
+            line_boxes_ids = [l.split(",") for l in line[1:]]
             # print(line[1:])
             boxes = np.array([np.array([float(xmin), float(ymin), float(xmax), float(ymax)])
                               for xmin, ymin, xmax, ymax, _ in line_boxes_ids])
@@ -438,7 +438,7 @@ class Trainer(object):
             line = line.split()
             image_gt = Image.open(line[0])
             image_dt = Image.open(line[0])
-            line_boxes_ids = [l.sprit(",") for l in line[1:]]
+            line_boxes_ids = [l.split(",") for l in line[1:]]
             gt_boxes = np.array([np.array([float(xmin), float(ymin), float(xmax), float(ymax)])
                               for xmin, ymin, xmax, ymax, _ in line_boxes_ids])
             gt_classes = np.array([int(class_id) for _, _, _, _, class_id in line_boxes_ids])
