9c8ff8e1c62af69d853ddabcb03358d9c6c4a725
iwasakishuto <Iwasaki-shuto0703@g.ecc.u-tokyo.ac.jp>
2020-02-20 09:52:42 +0000

    Using OpenCV to tracking.

--- tracker/opencv_tracker.py
+++ tracker/opencv_tracker.py
@@ -1,44 +1,20 @@
 #coding: utf-8
 import cv2
 from yolo import YOLO
+from tracker import BaseTrackingYolo
 
-OPENCV_TRACKER_HANDLER = {
-    "mil": cv2.TrackerMIL_create(),
-    'kcf': cv2.TrackerKCF_create(),
-    'tld': cv2.TrackerTLD_create(),
-    'median_flow': cv2.TrackerMedianFlow_create(),
-    'goturn': cv2.TrackerGOTURN_create(),
+OPENCV_TRACKER_CREATER = {
+    'boosting': cv2.TrackerBoosting_create,
+    'mil': cv2.TrackerMIL_create,
+    'kcf': cv2.TrackerKCF_create,
+    'tld': cv2.TrackerTLD_create,
+    'median_flow': cv2.TrackerMedianFlow_create,
+    'goturn': cv2.TrackerGOTURN_create,
+    'mosse': cv2.TrackerMOSSE_create,
+    'csrt': cv2.TrackerCSRT_create,
 }
 
-class OpenCVyolo(YOLO):
-    def __init__(self, method='kcf', **kwargs):
-        self.tracker = OPENCV_TRACKER_HANDLER.get(method.lower())
-        super().__init__(**kwargs)
-
-    def infer_video(self, video_path):
-        pass
-
-    
-
-    def video_inference(self, video_path):
-        # When the `video_path` directory has the sequential images.
-        if os.path.isdir(video_path):
-            self._seq_image_inference(video_path)        
-        # When the `video_path` is the video (.mp4, .mov)
-        else:
-            self._seq_image_inference(video_path)
-
-    def _video_data_inference(self, video_path):
-        video = cv2.VideoCapture(video_path)
-
-        im_w = video.get(cv2.CAP_PROP_FRAME_WIDTH)
-        im_h = video.get(cv2.CAP_PROP_FRAME_HEIGHT)
-        fps  = video.get(cv2.CAP_PROP_FPS)
-        frame_num = video.get(cv2.CAP_PROP_FRAME_COUNT)
-        int_ms = 1000 // int(fps)
-        
-
-
-    def _seq_image_inference(self, ):
-        pass
-        
+class OpenCVyolo(BaseTrackingYolo):
+    def __init__(self, method='kcf', reliability=0.7, **kwargs):
+        super().__init__(reliability=reliability, **kwargs)
+        self.Tracker_create = OPENCV_TRACKER_CREATER.get(method.lower())
